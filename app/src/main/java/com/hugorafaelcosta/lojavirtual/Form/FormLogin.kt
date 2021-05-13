ab1daf601435b09381a6692c268f83fb93191ebc
Hugo Costa <costarafaelhugo@gmail.com>
2021-05-13 11:30:22 -0300

    i made the configuration of slides and add the form activity

---
+++ app/src/main/java/com/hugorafaelcosta/lojavirtual/Form/FormLogin.kt
@@ -0,0 +1,12 @@
+package com.hugorafaelcosta.lojavirtual.Form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hugorafaelcosta.lojavirtual.R
+
+class FormLogin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_login)
+    }
+}
--- app/src/main/java/com/hugorafaelcosta/lojavirtual/Slides.kt
+++ app/src/main/java/com/hugorafaelcosta/lojavirtual/Slides.kt
@@ -1,30 +1,46 @@
 package com.hugorafaelcosta.lojavirtual
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.heinrichreimersoftware.materialintro.app.IntroActivity
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
+import com.hugorafaelcosta.lojavirtual.Form.FormLogin
 
 class Slides : IntroActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_slides)
 
+        isButtonBackVisible = false
+        isButtonNextVisible=false
+
         addSlide(
             SimpleSlide.Builder()
                 .background(R.color.roxo)
                 .image(R.drawable.drawer)
+                .backgroundDark(R.color.white)
                 .title("Loja Online de Calçados")
                 .description("Entre e confira as promoções que preparamos para você!")
                 .build()
+
         )
 
         addSlide(
             SimpleSlide.Builder()
                 .background(R.color.AV)
+                .canGoBackward(false)
                 .title("Crie uma conta Grátis")
                 .description("Cadastre-se agora mesmos! E venha conhecer os nossos produtos.")
                 .build()
         )
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        var intent=Intent(this, FormLogin::class.java)
+        startActivity(intent)
+        finish()
     }
 }
