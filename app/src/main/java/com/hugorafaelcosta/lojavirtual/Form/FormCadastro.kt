4d49c8080ae70bd0148cccd2000f902cd06c8aea
Hugo Costa <costarafaelhugo@gmail.com>
2021-06-01 08:59:00 -0300

    Finalizei o login da tela de castro de novo usuário

---
+++ app/src/main/java/com/hugorafaelcosta/lojavirtual/Form/FormCadastro.kt
@@ -0,0 +1,14 @@
+package com.hugorafaelcosta.lojavirtual.Form
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hugorafaelcosta.lojavirtual.R
+
+class FormCadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_cadastro)
+
+        supportActionBar!!.hide()
+    }
+}
--- app/src/main/java/com/hugorafaelcosta/lojavirtual/Form/FormLogin.kt
+++ app/src/main/java/com/hugorafaelcosta/lojavirtual/Form/FormLogin.kt
@@ -1,8 +1,10 @@
 package com.hugorafaelcosta.lojavirtual.Form
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.hugorafaelcosta.lojavirtual.R
+import kotlinx.android.synthetic.main.activity_form_login.*
 
 class FormLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,5 +12,10 @@ class FormLogin : AppCompatActivity() {
         setContentView(R.layout.activity_form_login)
 
         supportActionBar!!.hide()
+
+        text_tela_cadastro.setOnClickListener{
+            var intent = Intent(this,FormCadastro::class.java)
+            startActivity(intent)
+        }
     }
 }
